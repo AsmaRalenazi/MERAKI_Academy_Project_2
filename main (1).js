4473e24b198e5eb450f0fc65bfe1f34c5d19e886
AsmaRalenazi <AsmaAlenazi13@gmail.com>
2021-04-30 09:49:44 +0300

    edit second page

--- main (1).js
+++ main (1).js
@@ -1,22 +1,20 @@
-// $(window).on("load", function () {
+$(window).on("load", function () {
 
-//         $(`#p2Contaner`).hide();
-// });
-
-
-const buttonNext=$("#btn");
-buttonNext.text("next")
-
-buttonNext.click(()=>{
-
-
-    $(`#container`).hide();
-    $(`#p2Contaner`).show();
-
-    getMatch()
-
+        $(`#p2Contaner`).hide();
 });
 
+
+const buttonNext=$("#tswaqi_famel");
+
+buttonNext.click(()=>{
+    $(`#container`).hide();
+    $(`#p2Contaner`).show();
+    
+    
+});
+
+// getMatch()
+// buttonNext.text("next")
 // $(`#list-item-pushed${i}`).on("click", function () {
 //     $(this).css({
 //       color: "red",
