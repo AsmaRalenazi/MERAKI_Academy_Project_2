4962b8484963e8f09699694ade370f9b5d42fea6
AsmaRalenazi <AsmaAlenazi13@gmail.com>
2021-05-03 23:34:09 +0300

    edit code js for add to cart

--- main (1).js
+++ main (1).js
@@ -18,8 +18,8 @@ homepage.click(() => {
   $(`#p2Contaner`).hide();
 });
 
-let carts = $(`.add_to_cart`);
-console.log(carts);
+// let carts = $(`.add_to_cart`);
+// console.log(carts);
 // for (let i = 0; i < carts.length; i++) {
 //   carts[i].on(`click()`, () => {
 //     cartNumbers(products[i]);
@@ -30,6 +30,13 @@ let products = [
   {
     name: "AVALIERI",
     tag: "ساعة يد دائرية للنساء مع سوار من افاليري",
+
+    detail: `  "ساعة يد أنيقة من افاليري تمنح إطلالتك لمسة مميزة وتزين يديك بسوار فاخر لمزيد من الأناقة."
+    "تتميز بسوار شبكي مقاوم للصدأ ويعطي إحساسًا مريحًا لمعصمك وشكلًا انيقًا على يدك."
+    "بالإضافة للهيكل بحجم 30ملم، يتميز بمقاومته للماء حتى عمق 5متر لتحافظ على ساعتك وتبقيها محمية دائمًا"
+    "الهيكل يحتوي على عقربين، والميناء مزين بشعار افاليري، بالإضافة لنمط الأرقام الرومانية."
+   " هذه الساعة ستضيف لمسة من الفخامة لوقتك."   `
+   ,
     price: "621ريال",
     imge: "1.png",
   },
@@ -37,80 +44,60 @@ let products = [
   {
     name: "Kylemore",
     tag: " ساعة نسائية من كايلمور - فضي مع ذهبي",
+    detail:"",
     price: "321 ريال",
     imge: "2.png",
   },
   {
     name: "AVALIERI",
     tag: "ساعة نسائية من افاليري - ذهبي",
+    detail:"",
     price: "388 ريال",
     imge: "3.png",
   },
   {
     name: "jad",
     tag: `shum kshzdm`,
+    detail:"",
     price: "54565",
     imge: "4.png",
   },
 ];
-
+let counter=0
 function cartNumbers(product) {
   console.log(product);
+  counter++
+  localStorage.setItem("counter", counter);
+  
   // let productsNumbers = localStorage.getItem("cartNumbers");
 
   // productsNumbers = parseInt(productsNumbers);
   // if (productsNumbers) {
-  //   localStorage.setItem("cartNumbers", productsNumbers + 1);
+    localStorage.setItem("cartNumbers", productsNumbers + 1);
   // } else {
   //   localStorage.setItem("cartNumbers", 1);
   // }
   // setItems(product);
 }
+const carts=$("shopping")
+carts.click(()=>{
+  cartNumbers()
+})
+localStorage.getItem()
 
-// function srtItems(product) {}
-// const body=$('body')
-// const div=$('<div id ="div1"></div>');//creat new div in html
-// div.appendTo(body1);//add to html page
-// const name=$("<li>hi</li>");
-// name.appendTo(div);
-
-// const body1=$('body')
-// const div=$('<div id =class"p-box"></div>');//creat new div in html
-// div.appendTo(body1);//add to html page
 
 const detail_products = $(`#detail_products`);
-
-// const addToDetailpage = () => {
-//   const elem = $(`<div id="x"  class="p-box"> </div>`);
-//   const elem3 = $(` <div id="img${0}" class="imge1C">
-//       <img src="${products[0].imge} " id="${
-//     products[0].name
-//   }" class="AVALIERIC" />
-//       <p class="p2imge_AVALIERIC">${products[0].name}</p>
-//       <p class="p3imge_AVALIERIC">${products[0].tag}</p>
-//       <p class ="detail">ssssfffffspdofld,cllklk</p>
-//       <p id="price${0}" class="price">${products[0].price}</p>
-//       <a class="add_to_cart" href="#" onclick="cartNumbers(0)">اضف لسلة التسوق</a>
-//       </div>`);
-
-//   elem3.appendTo(elem);
-//   elem.appendTo(detail_products);
-// };
-
 const fun1 = (i) => {
-  // console.log("ddddddddddd", i);
-  const elem = $(`<div id="x"  class="p-box"> </div>`);
   const elem3 = $(` <div id="img${i}" class="imge1C">
       <img src="${products[i].imge} " id="${products[i].name}" class="AVALIERCC" />
-      <p class="p2imge_AVALIERIC">${products[i].name}</p>
+      <p class="AVALIERIC1">${products[i].name}</p>
       <p class="p3imge_AVALIERIC">${products[i].tag}</p>
-      <p class ="detail">ssssfffffspdofld,cllklk</p>
+      <p class ="detail">${products[i].detail}</p>
       <p id="price${i}" class="price">${products[i].price}</p>
       <a class="add_to_cart" href="#" onclick="cartNumbers(i)">اضف لسلة التسوق</a>
       </div>`);
 
-  elem3.appendTo(elem);
-  elem.appendTo(detail_products);
+  elem3.appendTo(detail_products);
   $(`#p2Contaner`).hide();
   $(`#detail_products`).show();
 };
@@ -133,14 +120,3 @@ addTomain = () => {
     elem.appendTo(all_products);
   }
 };
-
-// const pr= $("#AVALIERI");
-// pr.mouseover(function () {
-//   $(".AVALIERIC").css("cursor", "pointer");
-// });
-// pr.click(()=>{
-//   console.log("ddddddddddd");
-//   $(`#detail_products`).show();
-//   $(`#p2Contaner`).hide();
-//   addToDetailpage();
-// })
